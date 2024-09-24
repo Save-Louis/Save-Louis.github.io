3e471e9f15f121ccfdcf55aea2bba6dae37ef1b3
Save-Louis <donutaskyt@gmail.com>
2024-09-24 18:02:06 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('[autocomplete=one-time-code');
+input.addEventListener('input', () => input.style.setProperty('--_otp-digit', input.selectionStart));
+
+const wrongAudio = new Audio('/Wrong.mp3');
+let wrongAnswers = 0;
+
+function submit() {
+    if (input.value.length != 7) {
+        return;
+    }
+
+    if (input.value.toUpperCase() == "SKIBIDI") {
+        document.location = "/win.html";
+    } else if (wrongAnswers > 3) {
+        document.location = "/lose.html";
+    }
+    else {
+        wrongAnswers++;
+        wrongAudio.play();
+    }
+}
